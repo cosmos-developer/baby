029fe869b1161d2c99296a83c53f429fb7b2d092
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2022-06-15 12:03:20 +0700

    add docker exec in check-chain-id

--- homework/check-chain-id.sh
+++ homework/check-chain-id.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CHAIN_ID=$(babyd status --node tcp://localhost:1711 | jq ".NodeInfo.network" -r)
+CONTAINER_NAME="baby-node-1"
+CHAIN_ID=$(docker exec -i $CONTAINER_NAME babyd status --node http://localhost:1711 | jq ".NodeInfo.network" -r)
 if [ $CHAIN_ID == "toddler" ]
 then 
     echo "success: chain id is toddler"
